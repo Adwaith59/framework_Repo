b9ad2f5da27094bee07a0c772d0501b027c2b568
adwaith manoj <adwaith manoj@ADWAITH>
2026-09-23 15:22:41 +0530

    CHANGE

--- Vtiger2/src/test/java/pac1/createContactTest.java
+++ Vtiger2/src/test/java/pac1/createContactTest.java
@@ -6,6 +6,16 @@ public class createContactTest {
 	@Test
 	public void m1() {
 		System.out.println("execute createContactTest");
+		
+		String URL = System.getProperty("url");
+		String BROWSER = System.getProperty("browse", "chrome");
+		String USERNAME = System.getProperty("username");
+		String PASSWORD = System.getProperty("password");
+		
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
 	}
 	public class modifyContactTest {
 		@Test
--- Vtiger2/src/test/java/pac2/OrgTest.java
+++ Vtiger2/src/test/java/pac2/OrgTest.java
@@ -7,11 +7,20 @@ public class OrgTest {
 	public void createOrgTest()
 	{
 		System.out.println("execute createOrgTest");
+		String URL = System.getProperty("url");
+		String BROWSER = System.getProperty("browse", "chrome");
+		String USERNAME = System.getProperty("username");
+		String PASSWORD = System.getProperty("password");
+		
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
 	}
 	@Test
 	public void modifyOrgTest()
 	{
-		System.out.println("execute modifyOrgTest");
+		System.out.println("execute modifyOrgTestIII");
 	}
 
 }
