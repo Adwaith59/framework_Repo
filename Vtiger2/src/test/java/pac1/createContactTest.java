1178d1804d9d8e02a3aa9a95281357f30b74faf5
adwaith manoj <adwaith manoj@ADWAITH>
2026-09-23 12:14:31 +0530

    FIRST PUSH

---
+++ Vtiger2/src/test/java/pac1/createContactTest.java
@@ -0,0 +1,17 @@
+package pac1;
+
+import org.testng.annotations.Test;
+
+public class createContactTest {
+	@Test
+	public void m1() {
+		System.out.println("execute createContactTest");
+	}
+	public class modifyContactTest {
+		@Test
+		public void m1() {
+			System.out.println("execute modifyContactTest");
+		}
+	}
+}
+
